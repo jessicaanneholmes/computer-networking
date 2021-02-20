37a847d9102261f1245c8ab8079dc7867c9e3c1a
jessicaanneholmes <78622980+jessicaanneholmes@users.noreply.github.com>
2021-02-20 11:02:02 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -63,7 +63,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Send message data.
     # Fill in start
     clientSocket.send(msg.encode())
-    recv5 = clientSocket.recv(1024).decode()
+   # recv5 = clientSocket.recv(1024).decode()
     #print(recv5)
     #if recv5[:3] != '250':
         #print('250 reply not received from server.')
